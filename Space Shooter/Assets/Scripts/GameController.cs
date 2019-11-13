c0cf1260d9b825d65553210e82a9450d1a3dc841
Rickey <rickey9.rb@gmail.com>
2019-11-13 18:00:51 -0500

    Challenge3

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+	public Text winText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start ()
+	{
+	    gameOver = false;
+	    restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		winText.text = "";
+	    score = 0;
+		UpdateScore();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.T))
+			{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+	    yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+			    GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'T' to Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Points: " + score;
+		 if (score >= 100)
+          {
+            winText.text = "You Win! Game Created by Richard Barnes";
+            gameOver = true;
+            restart = true;
+			gameOverText.text = "";
+           }
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over! Game Created by Richard Barnes";
+		gameOver = true;
+	}
+}
